63cdc1f1e42822bd8920d2e11a98eb33852d53d6
Doug Harris <dharris@truthinitiative.org>
2019-11-05 10:07:31 -0500

    Rather than removing the element, change its position to static.
    This allows site headers to exist but in a less obtrusive location.

--- background.js
+++ background.js
@@ -4,14 +4,14 @@ const code = `(() => {
     const el = elements[i]
     const { display, position } = window.getComputedStyle(el)
     if ((position === 'fixed' || position === 'sticky') && display !== 'none') {
-      el.parentNode.removeChild(el)
+      el.style.position = 'static';
     }
   }
   const fix = '; overflow: visible !important; position: relative !important'
   document.body.style.cssText += fix
   document.documentElement.style.cssText += fix
-})()`
+})()`;
 
 chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.executeScript({ code })
-})
+  chrome.tabs.executeScript({ code });
+});
